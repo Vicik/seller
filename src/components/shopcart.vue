bff7082dc7f97826166537cdc817d105ddb6c26f
Vicik <sprung@126.com>
2019-04-24 09:46:48 +0800

    update

--- src/components/shopcart.vue
+++ src/components/shopcart.vue
@@ -97,7 +97,8 @@
       // height: 16px
       // border-radius: 50%
       // background: rgb(0, 160, 220)
-      transition: all 0.4s cubic-bezier(0.49,-0.29,0.75,0.41)
+      &.drop-enter-active
+        transition: all 0.4s cubic-bezier(0.49,-0.29,0.75,0.41)
       .inner
         width: 16px
         height: 16px
